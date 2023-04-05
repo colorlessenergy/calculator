096ec2df081bad96863a324c055b84cf0f236eb2
colorlessenergy <brianmunozdev@gmail.com>
2023-04-05 16:18:44 -0700

    add font

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,18 @@
-import '@/styles/globals.css'
+import '@/styles/globals.css';
+
+import { IBM_Plex_Mono } from 'next/font/google';
+
+const mono = IBM_Plex_Mono({ weight: '400', subsets: ['latin'] });
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+    return (
+        <>
+            <style jsx global>{`
+                html {
+                    font-family: ${mono.style.fontFamily};
+                }
+            `}</style>
+            <Component {...pageProps} />
+        </>
+    );
 }
