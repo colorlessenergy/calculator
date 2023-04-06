300a410d26ded6e4aa0309378a1a1c8b484523df
colorlessenergy <brianmunozdev@gmail.com>
2023-04-05 17:32:14 -0700

    add design styles

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,4 @@
 import Head from 'next/head';
-import Image from 'next/image';
 
 export default function Home() {
     return (
@@ -13,6 +12,31 @@ export default function Home() {
                 />
                 <link rel="icon" href="/favicon.ico" />
             </Head>
+            <div className="container">
+                <div className="output">100</div>
+
+                <div className="operations">
+                    <button className="operation operation-add">+</button>
+                    <button className="operation operation-subtract">-</button>
+                    <button className="operation operation-multiply">x</button>
+                    <button className="operation operation-divide">/</button>
+                </div>
+
+                <div>
+                    <button className="number">1</button>
+                    <button className="number">2</button>
+                    <button className="number">3</button>
+                    <button className="number">4</button>
+                    <button className="number">5</button>
+                    <button className="number">6</button>
+                    <button className="number">7</button>
+                    <button className="number">8</button>
+                    <button className="number">9</button>
+                    <button className="number number-clear">c</button>
+                    <button className="number">0</button>
+                    <button className="number">=</button>
+                </div>
+            </div>
         </>
     );
 }
